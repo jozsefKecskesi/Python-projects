ff22871eb9e836349c7e2b3c259674b616f3c8de
Jozsef Kecskesi <jozsef.kecskesi@live.ie>
2022-10-14 16:02:14 +0100

    Text and numbers practice.

--- PythonBasics/basics/textAndNum.py
+++ PythonBasics/basics/textAndNum.py
@@ -35,3 +35,28 @@ print("You have", slicesLeft, "slices remaining")
 age = int(input("How old are you? "))
 newAge = age + 1
 print(firstname, surname, "in your next birthday you will be", newAge, "years old.")
+
+# How much does it cost per head if we split the bill
+bill = int(input("What is the total cost of the bill? "))
+people = int(input("How many people are there? "))
+each = bill / people
+print("Each people should pay ", each)
+
+# Number of days exchanged to hours, minutes and seconds.
+days = int(input("Enter the number of days: "))
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print("In ", days, " days, there are:\n", hours, " hours, and\n", minutes, " minutes,\n", seconds, " seconds.", sep='')
+
+# Exchange kg to pound
+kilo = int(input("Enter the number of kilos: "))
+pound = kilo * 2.204
+print("That is", pound, "pounds.")
+
+# How many times a small number goes into a bigger one, and what is the remainder
+larger = int(input("Enter a number over 100: "))
+smaller = int(input("Enter a number under 10: "))
+calc3 = larger // smaller
+calc4 = larger % smaller
+print(smaller, "goes into", larger, calc3, "times, and the remainder is", calc4)
