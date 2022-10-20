ed7e952b33f60c5d092f59f228f838fe439d8073
Jozsef Kecskesi <jozsef.kecskesi@live.ie>
2022-10-20 15:30:28 +0100

    New code bits added.

--- PythonBasics/maths/maths.py
+++ PythonBasics/maths/maths.py
@@ -12,3 +12,30 @@ print(round(answer1, 2))
 
 print(round(math.pi, 5))
 
+radius = int(input("Enter the radius of the circle: "))
+depth = int(input("Enter depth: "))
+area = round((math.pi * (radius ** 2)), 3)
+volume = round((area * depth), 3)
+print(area, volume)
+
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter another number: "))
+ans3 = num3 // num4
+ans4 = num3 % num4
+print(num3, "divided by", num4, "is", ans3, "with", ans4, "remaining.")
+
+print("1, Square")
+print("2, Triangle\n")
+
+menuselection = int(input("Enter a number: "))
+if menuselection == 1:
+    side = int(input("Enter the lenght of one side: "))
+    area = side * side
+    print("The area of your Square is", area)
+elif menuselection ==2:
+    base = int(input("Enter the lenght of the base: "))
+    height = int(input("Enter the height of the triangle: "))
+    area = (base * height) / 2
+    print("The area of your Triangele is", area)
+else:
+    print("Incorrect option selected.")
---
+++ forloop/forloop.py
@@ -0,0 +1,27 @@
+# Print numbers 1-10
+for i in range(1, 11):
+    print(i)
+
+# Print numbers 1-10, with 3 as increasement
+for i in range(1, 11, 3):
+    print(i)
+
+# Print numbers between 1-10, with 2 as decreasement (descending order)
+for i in range(10, 1, -2):
+    print(i)
+
+# Print a word's letters, each in a new row
+word = "kismiska"
+for i in word:
+    print(i)
+
+name = input("Type in your name: ")
+for i in range(0, 3):
+    print(name)
+
+num = int(input("Enter a number: "))
+for i in range (0, num):
+    print(name)
+
+for i in name:
+    print(i)
