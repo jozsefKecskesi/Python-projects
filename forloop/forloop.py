23b21a35e68ba982ed7e732b624f2e78a54b7571
Jozsef Kecskesi <jozsef.kecskesi@live.ie>
2022-10-21 15:18:11 +0100

    If code example bits added.

--- forloop/forloop.py
+++ forloop/forloop.py
@@ -25,3 +25,52 @@ for i in range (0, num):
 
 for i in name:
     print(i)
+
+for x in range(0, num):
+    for i in name:
+        print(i)
+
+num1 = int(input("Enter a number between 1 and 12: "))
+for i in range(1, 13):
+    answer = i * num1
+    print(i, "x", num1, "=", answer)
+
+num2 = int(input("Enter a number below 50: "))
+for i in range(50, num2 - 1,  -1):
+    print(i)
+
+num3 = int(input("Enter a number: "))
+if num3 < 10:
+    for i in range(0, num3):
+        print(name)
+else:
+    for i in range(0, 3):
+        print("Too high.")
+
+total = 0
+for i in range(0, 5):
+    num4 = int(input("Enter a number: "))
+    ans = input("Do you want this number included? (y/n) ")
+    if ans == "y":
+        total = total + num4
+print(total)
+
+direction = input("Do you want to count up or down? (u/d) ")
+if direction == "u":
+    num5 = int(input("What is the top number? "))
+    for i in range(1, num5 + 1):
+        print(i)
+elif direction == "d":
+    num5 = int(input("Enter a number below 20: "))
+    for i in range(20, num5 - 1, -1):
+        print(i)
+else:
+    print("I don't understand.")
+
+num6 = int(input("How many friends do you want to invite to the party? "))
+if num6 <10:
+    for i in range(0, num6):
+        name = input("Enter a name: ")
+        print(name, "has been invited.")
+else:
+    print("Too many people.")
